14fcd99c01055e283aa5f6b22229024704f3656d
RocioNima <pilar.sanny12@gmail.com>
2023-03-08 00:19:24 -0500

    aprendiendo

---
+++ index.js
@@ -0,0 +1,21 @@
+// var amigos = ['neisser', 'rocio'];
+
+
+
+var amigos = [
+    { nombre: 'neisser', apellido: 'moreno', signo: 'escorpio' },
+    { nombre: 'Rocio', apellido: 'nima', signo: 'escorpio' }
+];
+
+var amigos2 = [
+    { nombre: 'elena', apellido: 'gonzales', signo: 'escorpio' },
+    { nombre: 'harry', apellido: 'romero', signo: 'escorpio' }
+];
+
+ var amigos3 = amigos.concat(amigos2);
+
+// amigos3.push({ nombre: 'Karen', apellido: 'Moreno', signo: 'sagitario' });
+
+// amigos3.pop();
+
+console.log(amigos3);
